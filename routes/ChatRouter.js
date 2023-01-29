b0023e746ab25e5ac17dc65f02234ad5ea4e35fe
fedinaimi <fedi.naimi@esprit.tn>
2023-01-29 22:26:58 +0100

    final

--- routes/ChatRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/ChatController');
-const router = express.Router()
-
-router.post('/', controller.createChat);
-router.get('/:userId', controller.userChats);
-router.get('/find/:firstId/:secondId', controller.findChat);
-
-module.exports = router 
--- routes/message.js
+++
@@ -1,7 +0,0 @@
-const { addMessage, getMessages } = require("../controllers/message");
-const router = require("express").Router();
-
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
-
-module.exports = router;
